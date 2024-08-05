2f77d38f56d6286157ce187fbed5b4f2b2fe1854
Peter <zatko.peter@igportals.eu>
2024-08-05 12:10:37 +0200

    - better

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React, { useState, useRef } from 'react';
 import html2pdf from 'html2pdf.js';
 import './App.css';
+import Handlebars from "handlebars";
 
 function App() {
   const [pdfBlob, setPdfBlob] = useState(null);
   const contentRef = useRef(null);
 
+  const data = { title: "Dynamic Title", message: "This is a dynamic message." };
+
   const convertToPdf = async () => {
-    const response = await fetch('/test.html');
-    const content = await response.text();
+    const response = await fetch('/test.hbs');
+    const templateSource = await response.text();
+
+    const template = Handlebars.compile(templateSource);
+    const renderedHtml = template(data);
 
     const options = {
       filename: 'my-document.pdf',
@@ -22,7 +28,7 @@ function App() {
       },
     };
 
-    html2pdf().set(options).from(content).save();
+    html2pdf().set(options).from(renderedHtml).save();
   };
 
   return (
